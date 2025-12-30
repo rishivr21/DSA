69bfa8a53a8d9f521c8f91229372f9f7956335a0
Rishikant Verma <rishikantver@gmail.com>
2025-12-30 22:48:47 +0530

    Time: 11 ms (6.97%) | Memory: 8.5 MB (67.36%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool isPalindrome(int x) 
+    {
+        
+        if(x<0){
+            return false;
+        }
+        int rem,temp=x;
+            long rev=0;
+    while(x!=0)
+        {
+          rem = x %10;
+          rev = rev*10 +rem;
+         x=x/10;
+        }
+        if(rev==temp ){
+            return true;
+        }
+        else{
+            return false;
+        }
+        
+    }
+};
