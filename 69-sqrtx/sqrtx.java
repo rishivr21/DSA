d671781cab523799300b877fb177acee7f14f1a5
Rishikant Verma <rishikantver@gmail.com>
2026-09-27 01:42:17 +0530

    Time: 1 ms (99.92%) | Memory: 42.9 MB (8.98%) - LeetSync

---
+++ 69-sqrtx/sqrtx.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int mySqrt(int x) {
+        if (x < 2) {
+            return x;
+        }
+
+        int start = 1;
+        int end = x / 2;
+        int ans = 0;
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+
+            if (mid <= x / mid) {
+                ans = mid;
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+
+        return ans;
+    }
+}
